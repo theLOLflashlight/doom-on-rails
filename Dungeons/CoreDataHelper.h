1137ac75535de781d24c591a88097a37ba4d4473
theLOLflashlight <andrew.t.meckling@gmail.com>
2016-04-13 20:19:02 -0700

    By Jacob: High scores

---
+++ Dungeons/CoreDataHelper.h
@@ -0,0 +1,21 @@
+//
+//  CoreDataHelper.h
+//  Dungeons
+//
+//  Created by Andrew Meckling on 2016-04-13.
+//  Copyright © 2016 Andrew Meckling. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@interface CoreDataHelper :NSObject
+
+@property (nonatomic, readonly) NSManagedObjectContext       *context;
+@property (nonatomic, readonly) NSManagedObjectModel         *model;
+@property (nonatomic, readonly) NSPersistentStoreCoordinator *coordinator;
+@property (nonatomic, readonly) NSPersistentStore            *store;
+
+- (void)setupCoreData;
+- (void)saveContext;
+@end
---
+++ Dungeons/CoreDataHelper.m
@@ -0,0 +1,15 @@
+//
+//  CoreDataHelper.m
+//  Dungeons
+//
+//  Created by Andrew Meckling on 2016-04-13.
+//  Copyright © 2016 Andrew Meckling. All rights reserved.
+//
+
+#import "CoreDataHelper.h"
+
+@implementation CoreDataHelper
+
+
+
+@end
